b1819d8de418e120fbcd5d056b87fc00c82e3ca7
Brian Novak <brian.novak@me.com>
2018-02-28 11:58:44 -0700

    project submission uplaod

--- app.js
+++ app.js
@@ -10,83 +10,83 @@ app.use(cors());
 
 app.get("/people", (request, response, next) => {
     queries.listPeople().then(people => {
-        response.json({people});
+      response.json({people});
     }).catch(next);
 });
 
 app.get("/events", (request, response, next) => {
     queries.listEvents().then(events => {
-        response.json({events});
+      response.json({events});
     }).catch(next);
 });
 
 app.get("/app-data", (request, response, next) => {
     queries.listAppData().then(appData => {
-        response.json({appData});
+      response.json({appData});
     }).catch(next);
 });
 
 app.get("/people/:id", (request, response, next) => {
     queries.readPerson(request.params.id).then(person => {
         person
-            ? response.json({person})
-            : response.sendStatus(404);
+          ? response.json({person})
+          : response.sendStatus(404);
     }).catch(next);
 });
 
 app.get("/events/:id", (request, response, next) => {
     queries.readEvent(request.params.id).then(event => {
         event
-            ? response.json({event})
-            : response.sendStatus(404);
+          ? response.json({event})
+          : response.sendStatus(404);
     }).catch(next);
 });
 
 app.post("/events", (request, response, next) => {
-    queries.createEvent(request.body).then(event => {
-        response.status(201).json("Your event was added!");
+    queries.createEvent(request.body).then(() => {
+      response.status(201).json("Your event was added!");
     }).catch(next);
 });
 
 app.post("/people", (request, response, next) => {
-    queries.createPerson(request.body).then(person => {
-        response.status(201).json("Your friend was added!");
+    queries.createPerson(request.body).then(() => {
+      response.status(201).json("Your friend was added!");
     }).catch(next);
 });
 
 app.post("/app-data", (request, response, next) => {
     queries.createAppData(request.body).then(appData => {
-        response.status(201).json({appData: appData});
+      response.status(201).json({appData: appData});
     }).catch(next);
 });
 
 app.delete("/events/:id", (request, response, next) => {
-    queries.deleteEvent(request.params.id).then(res => {
-        response.status(204).json("Your event was removed!");
+    queries.deleteEvent(request.params.id).then(() => {
+      response.status(201).json("Your event was removed!");
     }).catch(next);
 });
 
 app.delete("/people/:id", (request, response, next) => {
-    queries.deletePerson(request.params.id).then(res => {
-        response.status(204).json("Your friend was removed!");
+    queries.deletePerson(request.params.id).then(() => {
+      response.status(201).json("Your friend was removed!");
     }).catch(next);
 });
 
 app.delete("/app-data/:id", (request, response, next) => {
     queries.deleteAppData(request.params.id).then(() => {
-        response.status(204);
+      response.status(201);
     }).catch(next);
 });
 
 app.put("/events/:id", (request, response, next) => {
-    queries.updateEvent(request.params.id, request.body).then(event => {
-        response.json(event);
+    queries.updateEvent(request.params.id, request.body).then(() => {
+			response.status(201).json("Your event was updated!");
     }).catch(next);
 });
 
 app.put("/people/:id", (request, response, next) => {
-    queries.updatePerson(request.params.id, request.body).then(person => {
-        response.json(person);
+    queries.updatePerson(request.params.id, request.body).then(() => {
+      response.status(201).json("Your friend was updated!");
     }).catch(next);
 });
 
